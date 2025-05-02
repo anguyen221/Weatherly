e77a10027a1838ac663596b61a5589027b1a7374
meoz23 <laboumyz@gmail.com>
2025-05-02 11:38:59 -0400

    Updated main.dart to align with themes

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,69 @@ import '../screens/login_screen.dart';
 import '../screens/home_screen.dart';
 import 'services/auth_service.dart';
 import 'firebase_options.dart';
+import '../screens/themes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const WeatherlyApp());
+
+  final savedTheme = await AppThemes.loadTheme();
+
+  runApp(WeatherlyApp(initialTheme: savedTheme));
 }
 
-class WeatherlyApp extends StatelessWidget {
-  const WeatherlyApp({super.key});
+class WeatherlyApp extends StatefulWidget {
+  final String? initialTheme;
+
+  const WeatherlyApp({super.key, this.initialTheme});
+
+  @override
+  State<WeatherlyApp> createState() => _WeatherlyAppState();
+}
+
+class _WeatherlyAppState extends State<WeatherlyApp> {
+  late ValueNotifier<String?> selectedTheme;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedTheme = ValueNotifier(widget.initialTheme);
+    AppThemes.selectedTheme = selectedTheme;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weatherly',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const AuthGate(),
+    return ValueListenableBuilder(
+      valueListenable: selectedTheme,
+      builder: (context, themeName, _) {
+        return MaterialApp(
+          title: 'Weatherly',
+          theme: ThemeData(primarySwatch: Colors.blue),
+          home: BackgroundWrapper(
+            themeName: themeName,
+            child: const AuthGate(),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BackgroundWrapper extends StatelessWidget {
+  final Widget child;
+  final String? themeName;
+
+  const BackgroundWrapper({
+    super.key,
+    required this.child,
+    required this.themeName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: AppThemes.getBackgroundDecoration(themeName),
+      child: child,
     );
   }
 }
