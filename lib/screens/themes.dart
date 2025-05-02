d4ba59464c7e3b7a300eb53ac739db05cb1f6c71
meoz23 <laboumyz@gmail.com>
2025-05-02 10:46:53 -0400

    Created themes.dart and added images

---
+++ lib/screens/themes.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppThemes {
+  static const String _themeKey = 'selected_background';
+
+  static const Map<String, String> themeImages = {
+    'blue': 'assets/backgrounds/blue.jpg',
+    'yellow': 'assets/backgrounds/yellow.png',
+    'green': 'assets/backgrounds/green.png',
+    'pink': 'assets/backgrounds/pink.jpg',
+  };
+
+  static BoxDecoration getBackgroundDecoration(String? themeName) {
+    final imagePath = themeImages[themeName];
+    if (imagePath != null) {
+      return BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(imagePath),
+          fit: BoxFit.cover,
+        ),
+      );
+    } else {
+      return const BoxDecoration(color: Colors.white);
+    }
+  }
+
+  static Future<void> saveTheme(String themeName) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_themeKey, themeName);
+  }
+
+  static Future<String?> loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_themeKey);
+  }
+}
