f303fb0a9bafb3ea2f6d1e6f436b5c85e6b50b6c
anguyen221 <anguyen221@student.gsu.edu>
2025-05-01 20:30:35 -0400

    Fixed issues with authentication state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/login_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/home_screen.dart';
+import 'services/auth_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const WeatherlyApp());
 }
 
@@ -19,8 +19,28 @@ class WeatherlyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Weatherly',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginScreen(),
-      debugShowCheckedModeBanner: false,
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: AuthService().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+        if (snapshot.hasData) {
+          return const HomeScreen();
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
   }
 }
